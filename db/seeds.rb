38091bd04f556d4ba4f57c3656f3b29cc4355fdd
[01;31m[K"name"[m[K: "Matthew Ramuta <[01;31m[K"email"[m[K: "matthew.g.ramuta@gmail.com>
2016-11-27 16:32:54 -0600

    do stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require 'faker'
 
 User.delete_all
 Entry.delete_all
+Comment.delete_all
 
 
 users = 50.times.map do
@@ -22,20 +23,10 @@ comments = Entry.all.each do |entry|
   end
 end
 
-child_comments = Entry.all.each do |entry|
-  rand(0..5).times.map do
-    Comment.create!( :body => Faker::Lorem.paragraph,:author_id  => rand(User.first.id..User.last.id),entry_id:entry.id, parent_comment_id:entry.comments.ids.sample)
-  end
-end
-
-child_comments = Entry.all.each do |entry|
-  rand(0..5).times.map do
-    Comment.create!( :body => Faker::Lorem.paragraph,:author_id  => rand(User.first.id..User.last.id),entry_id:entry.id, parent_comment_id:entry.comments.ids.sample)
-  end
-end
-
-child_comments = Entry.all.each do |entry|
-  rand(0..5).times.map do
-    Comment.create!( :body => Faker::Lorem.paragraph,:author_id  => rand(User.first.id..User.last.id),entry_id:entry.id, parent_comment_id:entry.comments.ids.sample)
+child_comments = 10.times do 
+  Entry.all.each do |entry|
+    rand(0..5).times.map do
+      Comment.create!( :body => Faker::Lorem.paragraph,:author_id  => rand(User.first.id..User.last.id),entry_id:entry.id, parent_comment_id:entry.comments.ids.sample)
+    end
   end
 end
